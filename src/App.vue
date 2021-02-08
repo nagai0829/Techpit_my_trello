ec50dce2301195b9d1f0aeda241b11b678ae1f92
nagai0829 <ke.seiya0829@gmail.com>
2021-02-09 06:24:04 +0900

    firstcommit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <!-- ★Boardコンポーネント呼び出し -->
+    <board />
+  </div>
+</template>
+
+<script>
+// ★インポート文を追加
+import Board from './components/Board.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Board
+  }
+}
+</script>
